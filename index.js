21128957df5ae9eb25e73157a869f29c4a54de5e
fajar <fajarfathirwan@gmail.com>
2018-05-15 13:53:05 +0700

    dropdown

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-// import Calculator from './Components/Calculator';
-import Login from './Components/Login';
-import Test from './Components/Test';
-import HideNav from './Components/HideNav';
-import Navigation from "./Navigation";
-import ShopingHome from './Components/ShopingHome';
 
-AppRegistry.registerComponent('test1', () => Navigation);
+AppRegistry.registerComponent('test1', () => App);
